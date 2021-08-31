97c2bf6bc8f15d60c25d7f044cae7d0706a20f91
FarhaD <80015595+FarhaD11011@users.noreply.github.com>
2021-08-31 11:28:56 -0500

    Add files via upload

---
+++ syntax/in_range.py
@@ -0,0 +1,30 @@
+# def in_range(nums, lowest, highest):
+#     """Print numbers inside range.
+
+#     - nums: list of numbers
+#     - lowest: lowest number to print
+#     - highest: highest number to print
+
+#     For example:
+
+#       in_range([10, 20, 30, 40], 15, 30)
+
+#     should print:
+
+#       20 fits
+#       30 fits
+#     """
+
+#     # YOUR CODE HERE
+
+
+# in_range([10, 20, 30, 40, 50], 15, 30)            
+
+
+def in_range(nums, lowest, highest):
+
+  for num in nums:
+    if num >= lowest and num <= highest:
+      print(f"{num} fits")
+
+in-range([10,15,20,25,30,35,40], 15, 40)
